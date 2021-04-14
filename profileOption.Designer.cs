72af62ba648d9e933d3d9602a1456fd2a27f5d34
Giang <463189@student.fontys.nl>
2021-04-14 12:12:05 +0200

    Change to single page and secure throw catch

--- profileOption.Designer.cs
+++ profileOption.Designer.cs
@@ -33,6 +33,13 @@ namespace MultipeProfileWinForm
             this.createNewProfileButton = new System.Windows.Forms.Button();
             this.listProfileBox = new System.Windows.Forms.ListBox();
             this.continueButton = new System.Windows.Forms.Button();
+            this.AgeLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.profileAddLabel = new System.Windows.Forms.Label();
+            this.comboAgeBox = new System.Windows.Forms.ComboBox();
+            this.inputNameBox = new System.Windows.Forms.TextBox();
+            this.addProfileButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // whoLabelText
@@ -76,11 +83,98 @@ namespace MultipeProfileWinForm
             this.continueButton.UseVisualStyleBackColor = true;
             this.continueButton.Click += new System.EventHandler(this.continueButton_Click);
             // 
+            // AgeLabel
+            // 
+            this.AgeLabel.AutoSize = true;
+            this.AgeLabel.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.AgeLabel.Location = new System.Drawing.Point(24, 231);
+            this.AgeLabel.Name = "AgeLabel";
+            this.AgeLabel.Size = new System.Drawing.Size(61, 14);
+            this.AgeLabel.TabIndex = 12;
+            this.AgeLabel.Text = "Leeftijd:";
+            this.AgeLabel.Visible = false;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nameLabel.Location = new System.Drawing.Point(24, 156);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(47, 14);
+            this.nameLabel.TabIndex = 11;
+            this.nameLabel.Text = "Naam:";
+            this.nameLabel.Visible = false;
+            // 
+            // profileAddLabel
+            // 
+            this.profileAddLabel.AutoSize = true;
+            this.profileAddLabel.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.profileAddLabel.Location = new System.Drawing.Point(12, 110);
+            this.profileAddLabel.Name = "profileAddLabel";
+            this.profileAddLabel.Size = new System.Drawing.Size(258, 31);
+            this.profileAddLabel.TabIndex = 10;
+            this.profileAddLabel.Text = "Profiel toevoegen";
+            this.profileAddLabel.Visible = false;
+            // 
+            // comboAgeBox
+            // 
+            this.comboAgeBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboAgeBox.FormattingEnabled = true;
+            this.comboAgeBox.Items.AddRange(new object[] {
+            "<11",
+            "12-17",
+            "18+"});
+            this.comboAgeBox.Location = new System.Drawing.Point(24, 249);
+            this.comboAgeBox.Name = "comboAgeBox";
+            this.comboAgeBox.Size = new System.Drawing.Size(74, 23);
+            this.comboAgeBox.TabIndex = 9;
+            this.comboAgeBox.Visible = false;
+            // 
+            // inputNameBox
+            // 
+            this.inputNameBox.Location = new System.Drawing.Point(24, 174);
+            this.inputNameBox.Name = "inputNameBox";
+            this.inputNameBox.PlaceholderText = "Naam";
+            this.inputNameBox.Size = new System.Drawing.Size(228, 23);
+            this.inputNameBox.TabIndex = 8;
+            this.inputNameBox.Visible = false;
+            // 
+            // addProfileButton
+            // 
+            this.addProfileButton.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.addProfileButton.Location = new System.Drawing.Point(183, 328);
+            this.addProfileButton.Name = "addProfileButton";
+            this.addProfileButton.Size = new System.Drawing.Size(82, 23);
+            this.addProfileButton.TabIndex = 7;
+            this.addProfileButton.Text = "Aanmaken";
+            this.addProfileButton.UseVisualStyleBackColor = true;
+            this.addProfileButton.Visible = false;
+            this.addProfileButton.Click += new System.EventHandler(this.addProfileButton_Click);
+            // 
+            // backButton
+            // 
+            this.backButton.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.backButton.Location = new System.Drawing.Point(24, 328);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 13;
+            this.backButton.Text = "Terug";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Visible = false;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
             // dataflixProfiles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.AgeLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.profileAddLabel);
+            this.Controls.Add(this.comboAgeBox);
+            this.Controls.Add(this.inputNameBox);
+            this.Controls.Add(this.addProfileButton);
             this.Controls.Add(this.continueButton);
             this.Controls.Add(this.listProfileBox);
             this.Controls.Add(this.createNewProfileButton);
@@ -98,6 +192,13 @@ namespace MultipeProfileWinForm
         private System.Windows.Forms.Button createNewProfileButton;
         private System.Windows.Forms.ListBox listProfileBox;
         private System.Windows.Forms.Button continueButton;
+        private System.Windows.Forms.Label AgeLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label profileAddLabel;
+        private System.Windows.Forms.ComboBox comboAgeBox;
+        private System.Windows.Forms.TextBox inputNameBox;
+        private System.Windows.Forms.Button addProfileButton;
+        private System.Windows.Forms.Button backButton;
     }
 }
 
--- profileOption.cs
+++ profileOption.cs
@@ -12,6 +12,7 @@ namespace MultipeProfileWinForm
 {
     public partial class dataflixProfiles : Form
     {
+        
         public dataflixProfiles()
         {
             InitializeComponent();
@@ -19,26 +20,89 @@ namespace MultipeProfileWinForm
 
         private void createNewProfileButton_Click(object sender, EventArgs e)
         {
-            profileCreation profileCreationWindow = new profileCreation(this);
-            profileCreationWindow.Show();
+            profileCreationUI();
         }
-
-        public string profileList
+        public void addProfileButton_Click(object sender, EventArgs e)
         {
-            get 
-            { 
-                return listProfileBox.Text; 
-            }
-            set 
+            string age = comboAgeBox.Text;
+            string name = inputNameBox.Text;
+
+            if (name == "" || age == "")
             {
-                listProfileBox.Text = value;
-                listProfileBox.Items.Add(listProfileBox.Text);
+                MessageBox.Show("Naam en/of leeftijd niet ingevuld");
             }
+            else
+            {
+                string profile = $"{name} | {age}";
+                
+                if (listProfileBox.Items.Contains(profile))
+                {
+                    MessageBox.Show($"U heeft al een naam die {name} heet");
+                }
+                else
+                {
+                    listProfileBox.Items.Add(profile);
+                    MessageBox.Show($"Profiel {name} is aangemaakt");
+                    profileOptionUI();
+                    resetInput();
+                }
+            }
+        }
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            profileOptionUI();
+            resetInput();
         }
 
         private void continueButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show($"Ingelogd in {listProfileBox.Text}");
+            if (listProfileBox.Text == "")
+            {
+                MessageBox.Show($"U heeft geen profiel gekozen {Environment.NewLine}Selecteer of maak een nieuwe profiel aan");
+            }
+            else
+            {
+                MessageBox.Show($"Profiel {listProfileBox.Text} gekozen");
+            }
+        }
+
+        public void profileCreationUI()
+        {
+            profileAddLabel.Visible = true;
+            nameLabel.Visible = true;
+            inputNameBox.Visible = true;
+            AgeLabel.Visible = true;
+            comboAgeBox.Visible = true;
+            addProfileButton.Visible = true;
+            backButton.Visible = true;
+
+            createNewProfileButton.Visible = false;
+            whoLabelText.Visible = false;
+            listProfileBox.Visible = false;
+            continueButton.Visible = false;
+            
+        }
+
+        public void profileOptionUI()
+        {
+            profileAddLabel.Visible = false;
+            nameLabel.Visible = false;
+            inputNameBox.Visible = false;
+            AgeLabel.Visible = false;
+            comboAgeBox.Visible = false;
+            addProfileButton.Visible = false;
+            backButton.Visible = false;
+
+            createNewProfileButton.Visible = true;
+            whoLabelText.Visible = true;
+            listProfileBox.Visible = true;
+            continueButton.Visible = true;
+        }
+
+        public void resetInput()
+        {
+            inputNameBox.Text = "";
+            comboAgeBox.SelectedIndex = -1;
         }
     }
 }
